64a59f1a5910d2878ba0e9ad485479100ba4cece
slopez2091 <steve.lopez2091@gmail.com>
2021-12-11 09:50:05 +0530

    'development-branch-commit'

---
+++ src/components/ExampleThree.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExampleThree = () => {
+    return (
+        <React.Fragment>
+            <h1>Hola from Example Three Component</h1>
+        </React.Fragment>
+    )
+}
+
+export default ExampleThree
