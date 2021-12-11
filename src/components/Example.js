2f75f14bd8bf406bdcc06076988486c57b0df0a2
slopez2091 <steve.lopez2091@gmail.com>
2021-12-11 09:38:00 +0530

    'example-component'

---
+++ src/components/Example.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Example = () => {
+    return (
+        <React.Fragment>
+            <h1>Hola There!!!</h1>
+        </React.Fragment>
+    )
+}
+
+export default Example
