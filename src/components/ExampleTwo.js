82ae1071a424dca2220a603ba2d8bf833c24d498
slopez2091 <steve.lopez2091@gmail.com>
2021-12-11 09:40:36 +0530

    'example-two-component'

---
+++ src/components/ExampleTwo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExampleTwo = () => {
+    return (
+        <React.Fragment>
+            <h1>Hello from Example Two Component</h1>
+        </React.Fragment>
+    )
+}
+
+export default ExampleTwo
